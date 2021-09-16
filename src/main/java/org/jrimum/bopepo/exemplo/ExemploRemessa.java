3a455ff8172af24239430bc5ec96f2fdedb2b330
Christiano Moura Pereiram <chris.engcomp@gmail.com>
2021-09-16 01:06:20 -0300

    .

--- src/main/java/org/jrimum/bopepo/exemplo/ExemploRemessa.java
+++ src/main/java/org/jrimum/bopepo/exemplo/ExemploRemessa.java
@@ -30,7 +30,7 @@ public class ExemploRemessa {
 	public static void main(String[] args) throws IOException {
 
 		// Identificando Layout
-		File layoutTemporario = new File("LayoutRemessa_BBCNAB400.txg.xml");
+		File layoutTemporario = new File("LayoutRemessa_DaycovalCNAB400.txg.xml");
 		File layout = Files.bytesToFile(layoutTemporario, IOUtils.toByteArray(ClassLoaders.getResourceAsStream("/layoutBanco/LayoutRemessa_DaycovalCNAB400.txg.xml")));
 
 		// Criando o arquivo de remessa
@@ -50,7 +50,7 @@ public class ExemploRemessa {
 		ff.addRecord(createTrailer(ff, sequencial));
 
 		// Salvando arquivo de remessa.
-		File arquivoDeRemessa = new File("resource/arquivoRemessa/REMESSABB"+new SimpleDateFormat("ddMMyyyy").format(new Date())+".REM");
+		File arquivoDeRemessa = new File("resource/arquivoRemessa/REMESSADaycoval"+new SimpleDateFormat("ddMMyyyy").format(new Date())+".REM");
 		FileUtils.writeLines(arquivoDeRemessa, ff.write(), "\r\n");
 		System.out.println("Arquivo gerado com sucesso em: "+ arquivoDeRemessa);
 
@@ -73,7 +73,7 @@ public class ExemploRemessa {
 		header.setValue("IdentificacaoTipoServico", 1);
 		header.setValue("IdentificacaoExtensoTipoServico", "COBRANCA");
 		*/
-		header.setValue("PrefixoAgencia", 1623);
+		/*header.setValue("PrefixoAgencia", 1623);
 		header.setValue("DigitoVerificadorAgencia", 2);
 		header.setValue("NumeroContaCorrente", 101916);
 		header.setValue("DigitoVerificadorContaCorrente", 0);
@@ -82,7 +82,14 @@ public class ExemploRemessa {
 		header.setValue("DataGravacao", 170118);
 		header.setValue("SequencialRemessa", "0000000");
 		header.setValue("NumeroConvenioLider", convenio);
-		header.setValue("SequencialRegistro", 1);
+		header.setValue("SequencialRegistro", 1);*/
+                
+                /*Exemplo Daycoval*/
+                header.setValue("CodigoEmpresa", "cleancoat"); 
+                header.setValue("NomeEmpresa","Cleancoat Chemical Ltda");
+                header.setValue("DataGravacao","150921");                       
+                //header.setValue("Filler", " ");
+                header.setValue("SequencialRegistro", 1) ; 
 
 		return header;
 	}
@@ -97,7 +104,6 @@ public class ExemploRemessa {
 		/*
 		transacaoTitulos.setValue("IdentiFicacaoRegistroDetalhe", 7);
 		transacaoTitulos.setValue("TipoInscricaoCedente", 02);
-		*/
 		transacaoTitulos.setValue("NumeroCpfCnpjCedente", "06313870000441");
 		transacaoTitulos.setValue("PrefixoAgencia", 1623);
 		transacaoTitulos.setValue("DigitoVerificadorAgencia", 2);
@@ -106,18 +112,14 @@ public class ExemploRemessa {
 		transacaoTitulos.setValue("NumeroCovenioCobrancaCedente", convenio);
 		//transacaoTitulos.setValue("CodigoControleEmpresa", "");
 		transacaoTitulos.setValue("NossoNumero", nDocumento+sequencial);
-		/*
 		transacaoTitulos.setValue("NumeroPrestacao", 00);
 		transacaoTitulos.setValue("GrupoValor", 00);
 		transacaoTitulos.setValue("IndicativoMensagemSacador", "");
 		transacaoTitulos.setValue("PrefixoTitulo", "   ");
-		*/
 		transacaoTitulos.setValue("VariacaoCarteira", "019");
-		/*
 		transacaoTitulos.setValue("ContaCaucao", 0);
 		transacaoTitulos.setValue("NumeroBordero", 000000);
 		transacaoTitulos.setValue("TipoCobranca", "     ");
-		*/
 		transacaoTitulos.setValue("CarteiraCobranca", 17);
 		transacaoTitulos.setValue("Comando", 01);
 		//transacaoTitulos.setValue("NumeroTituloAtribuidoCedente", "          ");
@@ -126,20 +128,16 @@ public class ExemploRemessa {
 		/*transacaoTitulos.setValue("NumeroBanco", 001);
 		transacaoTitulos.setValue("PrefixoAgenciaCobradora", 0000);
 		transacaoTitulos.setValue("DigitoVerificadorPrefixoAgenciaCobradora", " ");
-		*/
 		transacaoTitulos.setValue("EspecieTitulo", 12);
 		transacaoTitulos.setValue("AceiteTitulo", "N");
 		transacaoTitulos.setValue("DataEmissao", 170118);
-		/*
 		transacaoTitulos.setValue("InstrucaoCodificada", "  ");
 		transacaoTitulos.setValue("InstrucaoCodificada", "  ");
-		*/
 		transacaoTitulos.setValue("JurosMoraDiaAtraso", "00000000003");
-		/*transacaoTitulos.setValue("DataLimite", "      ");
+                transacaoTitulos.setValue("DataLimite", "      ");
  		transacaoTitulos.setValue("ValorDesconto", "           "); 
 		transacaoTitulos.setValue("ValorIof", "           ");
 		transacaoTitulos.setValue("ValorAbatimento", "           ");
-		*/
 		transacaoTitulos.setValue("TipoInscricaoSacado", 01);
 		transacaoTitulos.setValue("NumeroCnpjCpfSacado", "12345678900");
 		transacaoTitulos.setValue("NomeSacado", "FULADO DA SILVA SAURO");
@@ -149,12 +147,43 @@ public class ExemploRemessa {
 		transacaoTitulos.setValue("CepEnderecoSacado", "66075110");
 		transacaoTitulos.setValue("CidadeSacado", "BELÉM");
 		transacaoTitulos.setValue("UfCidadeSacado", "PA");
-		/*
 		transacaoTitulos.setValue("Observacao", "                                        ");
 		transacaoTitulos.setValue("NumeroDiasProtesto", "  ");
 		transacaoTitulos.setValue("ComplementoRegistro", " ");
-		*/
-		transacaoTitulos.setValue("SequencialRegistro", sequencial);
+		transacaoTitulos.setValue("SequencialRegistro", sequencial);*/
+                        
+                        
+                /*Exemplo Daycoval*/
+                transacaoTitulos.setValue("NumeroCpfCnpjCedente", "17430900000171");   
+                transacaoTitulos.setValue("CodigoEmpresa", "cleancoat"); 
+                transacaoTitulos.setValue("CodigoControleEmpresa", "123");
+                transacaoTitulos.setValue("NossoNumero", nDocumento+sequencial);
+                transacaoTitulos.setValue("NumeroPrestacao", 01); 
+                transacaoTitulos.setValue("DataVencimento", "200921");   
+                transacaoTitulos.setValue("ValorTitulo",100.50);
+                transacaoTitulos.setValue("PrefixoAgencia",0000);   
+                transacaoTitulos.setValue("DigitoVerificadorAgencia", 0); 
+                transacaoTitulos.setValue("EspecieTitulo",12);  
+                transacaoTitulos.setValue("AceiteTitulo","N");
+                transacaoTitulos.setValue("DataEmissao", "160921");  
+                transacaoTitulos.setValue("JurosMoraDiaAtraso", "00000000003");
+                transacaoTitulos.setValue("DescontoAte", "");
+                transacaoTitulos.setValue("ValorDesconto", 0);
+                transacaoTitulos.setValue("ValorAbatimento", 0);
+                transacaoTitulos.setValue("TipoInscricaoSacado",01) ;
+                transacaoTitulos.setValue("NumeroCnpjCpfSacado", "12345678900");  
+                transacaoTitulos.setValue("NomeSacado", "FULADO DA SILVA SAURO");  
+                transacaoTitulos.setValue("ComplementoRegistro",""); 
+                transacaoTitulos.setValue("EnderecoSacado", "Rua Augusto Corrêa, LABES");
+		transacaoTitulos.setValue("BairroSacado", "Guamá");
+		transacaoTitulos.setValue("CepEnderecoSacado", "66075110");
+		transacaoTitulos.setValue("CidadeSacado", "BELÉM");
+		transacaoTitulos.setValue("UfCidadeSacado", "PA");
+                transacaoTitulos.setValue("NomeAvalista", "");  
+                transacaoTitulos.setValue("ComplementoRegistro","");  
+                transacaoTitulos.setValue("DiasProtesto",0);
+                transacaoTitulos.setValue("Moeda",1);  
+                transacaoTitulos.setValue("SequencialRegistro", sequencial);
 		
 		return transacaoTitulos;
 	}
